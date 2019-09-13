f92b75b8cb443411556230499597a70497a4e926
Allen Simpson <allen.simpson@motionencoding.com>
2019-09-13 02:19:42 -0600

    added shuffling to glass.fsx and recorded the average errors and time for both non-shuffled  and shuffled data

--- F#Test/glass.fsx
+++ F#Test/glass.fsx
@@ -224,6 +224,110 @@ let newTrainingDataSet n =                                                      
         }
     )
 
+
+
+let newShuffledTrainingDataSet n =                                                                  //This is a variation on the trainingDataSet that we made to accomidate for continuous values for the attributes 
+
+    let shuffleAttributes () =                                                                  //This will generate a verson of the data that shuffles 10% of the attributes
+        let workingData =                                                                       //We're getting the data we will work with
+            System.IO.File.ReadAllLines(@"E:\Project 1\Data\2\glass.data")                      //get the data from file (yes this needs to match a directory that can read it)
+            |> Seq.map (fun line -> line.Split(',') |> Array.map (fun value -> value.Trim()))   //split the lines on the commas
+            |> Seq.map (fun sa ->                                                               //now we are taking each value and...
+                let i = sa.[0]                                                                  //the first value gets to be an ID
+                let cls = sa.[sa.Length-1]                                                      //the last value gets to be a CLS
+                let attribs = sa |> Seq.skip 1 |> Seq.take (sa.Length-2) |> Seq.toArray         //We take everything else, drop the first and last values and make the result into an array
+                (i,cls),attribs                                                                 //we are making a tuple of a tuple here 
+            )
+            |>Seq.toArray                                                                       //making the sequence into an array so we don't recalculate every time we call workingData
+
+        let shuffle (data:string [] []) attr=                                       //this will shuffle the attribute "attr" in the string array data
+            let mutable i = 0                                                       //we are doing this imperitvely as a show of force (this is how you make a mutable value)
+            let attributes = ResizeArray (data |> Seq.map (fun xs -> xs.[attr]))    //we are making an array of the values from data's 'attr'th attribute array
+            let rnd = System.Random()                                               //make our randomNumberGenerator
+            while attributes.Count>0 do                                             //while we have attributes...
+                let j = rnd.Next(0,attributes.Count)                                //get a random index in attributes
+                let v = attributes.[j]                                              //assign v the value of the j'th attribute out of attributes
+                attributes.RemoveAt(j)                                              //remove the j'th element from attributes
+                data.[i].[attr] <- v                                                //replace the value of the i'th data point's 'attr'th attribute (this effects the actual value of data outside the function)
+                i <- i+1                                                            //increment i
+
+        
+        let data = workingData |>Array.map snd                                                  //defining data as the attribute array from working Data
+        let modifyCount = (workingData.[0]|> snd |> Array.length |> float)*0.1 |> ceil |> int   //this is the count of modifiable attributes (literally the length of attributes*0.1 rounded up)
+        let attribsCount = (workingData.[0]|> snd |> Array.length)                              //this is the number of actual attributes
+        let rnd = System.Random()                                                               //make a randomNumberGenerator
+        let idxs = ResizeArray([0..(attribsCount-1)])                                           //we are making a mutable list of indicies 
+        List.init modifyCount (fun _ ->                                                         //make a new list with magnitude of modify count (the number of elements we are shuffling)
+            let j = rnd.Next(0,idxs.Count)                                                      //get a random index from idxs
+            let i = idxs.[j]                                                                    //let i be the random index
+            idxs.RemoveAt(j)                                                                    //we shall remove said index from idxs (so we don't choose it again)
+            i                                                                                   //and add it to our list
+        )                                                                                       ////This randomly chooses the attribute numbers we're going to shuffle
+        |>Seq.iter (shuffle data)                                                               //we now iter through this list of indecies and shuffle the data at the index (This shuffling modifies the actual values of data)
+        Seq.zip workingData data                                                                //then we make a tuple of the working data and the shuffled data
+        |>Seq.map (fun (((i, cls),oldData),newData) ->                                          //Then we take the form ((string,string),string[],string[])
+            seq {yield i; yield! newData; yield cls} |> String.concat "," )                     //and convert it into one long sequence of strings which we immediately concat with ','
+
+    
+    let data =
+        shuffleAttributes ()                                                                        //we start with the shuffled values this time
+        |> Seq.map (fun line -> line.Split(',') |> Array.map (fun value -> value.Trim()))           // this give you an array of elements from the comma seperated fields. We trim to make sure that any white space is removed.
+        |> Seq.filter (Seq.exists(fun f -> f="?") >> not)                                           //This filters out all lines that contain a "?"
+        |> Seq.map (fun sa ->                                                                       //here we are taking each array of strings (the attributes)
+            let i = sa.[0] |> System.Int32.Parse                                                    //parse the first element (the id) as an int
+            let cls = sa.[sa.Length-1] |> System.Int32.Parse                                        //parse the last element (the class) as an int
+            let attrs = sa|> Seq.skip 1 |>Seq.take 9 |> Seq.map System.Double.Parse |> Seq.toArray  //take the array, skip the first and take all but the last, parse each value as a double and make the result into an array
+            i,cls,attrs                                                                             //Then make a 3-tuple of the id,class,and attribute array
+        )
+    let splitIntoDivisions n xs =       //This creates a classifier function that takes an attribute seq and splits it into n divisions | result is a float -> int lambda
+        let min = xs |> Seq.min         //get the minimum attribute value
+        let max = xs |> Seq.max         //get the maximum attribute value
+        let width = (max-min)/(float n) //get the width of each class
+        fun v -> int ((v-min)/width)    //return a function that takes a float v and returns the division it lies in
+
+    let p = 
+        data                            //take the data (idx,cls,attribs[])
+        |> Seq.map (fun (_,_,xs)->xs)   //get the attribs
+        |> Seq.head                     //get the first set of attribs
+        |> Seq.length                   //and find out how many there are (they should all be the same so p is a constant)
+
+    let funcs =                                 //here we are making an array of functions for each attribute that will divide said attribute into n divisions
+        Array.init p (fun i ->                  //so first make an array of size p (the number of attributes)
+            data                                //take our data...
+            |> Seq.map (fun (_,_,xs)-> xs.[i])  //and extract out the ith attribute from all our datapoints into a single sequence
+            |> splitIntoDivisions n             //then have splitIntoDivisions generage a function for the ith attribute set of values 
+        )
+
+    data                                                        //Now take our data (idx,cls,attribs[])
+    |> Seq.map (fun (i,cls,xs) ->                               //create our Data class by doing the following:
+        {
+            ID = i                                              //ID is i 
+
+            RI = xs.[0] |> funcs.[0]                            //for each attribute in the attribute array,
+            NA2O = xs.[1] |> funcs.[1]                          //get the attribute's index, access that attribute value
+            MGO = xs.[2] |> funcs.[2]                           //and pass it to the attribute conversion function (funcs)
+            AL2O3 = xs.[3] |> funcs.[3]                         //this takes each individual attribute float and "classifies" it
+            SIO2 = xs.[4] |> funcs.[4]
+            K2O = xs.[5] |> funcs.[5]
+            CAO = xs.[6] |> funcs.[6]
+            BAO = xs.[7] |> funcs.[7]
+            FE2O3 = xs.[8] |> funcs.[8]
+
+            CLS = cls |> (fun x ->                              //Here we're matching cls with what class it represents
+                 match x with                                   //this could be done on line 207 (when defining cls) but
+                 | 1 -> BuildingWindowsFloatProcessed           //this format matches the same format we use in the other 
+                 | 2 -> BuildingWindowsNonFloatProcessed        //files
+                 | 3 -> VehicleWindowsFloatProcessed
+                 | 4 -> VehicleWindowsNonFloatProcessed
+                 | 5 -> Containers
+                 | 6 -> Tableware
+                 | 7 -> Headlamps
+                 | _   -> VehicleWindowsFloatProcessed          // Since there are none of these in the current dataset, this can serve as a default
+             )
+        }
+    )
+
+
 //classify trainingDataSet { id = 1018561; clumpT = 2; cellsizeuniform = 1; cellshapeuniform = 2; margadhesion = 1; SECS = 2; barenuclei = 1; blandchromatin = 3; normalnucleoli = 1; mitoses = 1; cls = Benign} // Run for result
 let sw = System.Diagnostics.Stopwatch.StartNew ()
 // Seq.init 10 id 
@@ -237,6 +341,23 @@ Seq.init 100 (fun k -> printfn "Working on %d..." (k+1); doKFold 10  trainingSet
 sw.Stop()
 printfn "%A" sw.Elapsed
 
+//Average error: 35.1340% +/- ~2%
+//time: 00:01:55.5431
+
+
+sw.Start ()
+// Seq.init 10 id 
+// |>Seq.iter (fun n ->
+let trainingSet2 = newShuffledTrainingDataSet (n)
+Seq.init 100 (fun k -> printfn "Working on %d..." (k+1); doKFold 10  trainingSet2)
+|>Seq.average
+|>printfn "Division: %d Average Loss: %f" (n+1)
+sw.Stop()
+printfn "%A" sw.Elapsed
+
+
+//Average error: 45.7632% +/- ~2%
+//time: 00:03:48.3773
 
 //As things stand right now, executing everything you will get a number between 0. and 1.0 (though most numbers lie between 0.0 and 0.1 with an average ~0.02) //This is a good number 2% is a low fail rate
 //This result gives the average % of failures for all validation sets.
